f2102656ef5a247f7286c31144476cc5f4466095
Abdul Hadi Shah <101188074+abdulhadishah@users.noreply.github.com>
2025-12-02 00:00:52 +0530

    Refactor jQuery for menu toggle and scrolling
    Refactor jQuery code for menu and smooth scrolling functionality.

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,26 @@
-$(document).ready(function(){
-
-    $('#menu').click(function(){
-      $(this).toggleClass('fa-times');
-      $('header').toggleClass('toggle');
-    });
-  
-    $(window).on('scroll load',function(){
-  
-      $('#menu').removeClass('fa-times');
-      $('header').removeClass('toggle');
-  
-      if($(window).scrollTop() > 0){
-        $('.top').show();
-      }else{
-        $('.top').hide();
-      }
-  
-    });
-  
-    // smooth scrolling 
-  
-    $('a[href*="#"]').on('click',function(e){
-  
-      e.preventDefault();
-  
-      $('html, body').animate({
-  
-        scrollTop : $($(this).attr('href')).offset().top,
-  
-      },
-        500, 
-        'linear'
-      );
-  
-    });
-  
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
   });
+
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
+  });
+
+  // smooth scrolling
+  $('a[href*="#"]').on("click", function (e) {
+    const target = $(this).attr("href");
+    if (target && target.startsWith("#")) {
+      e.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(target).offset().top,
+        },
+        500,
+        "linear"
+      );
+    }
+  });
+});
